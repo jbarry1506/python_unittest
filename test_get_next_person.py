ee96366005b7e219050331885a6bc00b99f83254
jbarry1506 <jbarry1506@gmail.com>
2019-09-16 11:24:56 -0400

    Create test_get_next_person.py

---
+++ test_get_next_person.py
@@ -0,0 +1,13 @@
+from mock import patch
+
+@patch("application.get_random_person")
+def test_new_person(mock_get_rand_person):
+    # arrange
+    user = {'people_seen': []}
+    expected_person = 'Katie'
+    mock_get_rand_person.return_value = 'Katie'
+    
+    # action
+    actual_person = get_next_person(user)
+    
+    # assert_equals(actual_person, expected_person)
