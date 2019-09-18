595c75842a6ef1efe3358d9e9ab597d4595b9179
jbarry1506 <jbarry1506@gmail.com>
2019-09-18 11:29:36 -0400

    Update test_get_next_person.py

--- test_get_next_person.py
+++ test_get_next_person.py
@@ -10,4 +10,4 @@ def test_new_person(mock_get_rand_person):
     # action
     actual_person = get_next_person(user)
     
-    # assert_equals(actual_person, expected_person)
+    assert_equals(actual_person, expected_person)
